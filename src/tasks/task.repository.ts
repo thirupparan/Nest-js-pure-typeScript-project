f5b0082a84a55d849e87ac71d42dea8e6423350b
thirupparan <thirupparan1994@gmail.com>
2019-10-16 12:51:34 +0530

    problem solve

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,14 +1,15 @@
-import { Repository, EntityRepository } from 'typeorm';
 import { Task } from './task.entity';
+import { EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { User } from '../auth/user.entity';
-import { TaskStatus } from './task-status.enum';
 import { Logger, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
   private logger = new Logger('TaskRepository');
+
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
@@ -25,6 +26,7 @@ export class TaskRepository extends Repository<Task> {
         { search: `%${search}%` },
       );
     }
+
     try {
       const tasks = await query.getMany();
       return tasks;
@@ -32,7 +34,7 @@ export class TaskRepository extends Repository<Task> {
       this.logger.error(
         `Failed to get tasks for user "${
           user.username
-        }", Filters:${JSON.stringify(filterDto)}`,
+        }". Filters: ${JSON.stringify(filterDto)}`,
         error.stack,
       );
       throw new InternalServerErrorException();
@@ -47,11 +49,12 @@ export class TaskRepository extends Repository<Task> {
     task.description = description;
     task.status = TaskStatus.OPEN;
     task.user = user;
+
     try {
       await task.save();
     } catch (error) {
       this.logger.error(
-        `Failed to create task for user "${user.username}". Data: ${createTaskDto}`,
+        `Failed to create a task for user "${user.username}". Data: ${createTaskDto}`,
         error.stack,
       );
       throw new InternalServerErrorException();
